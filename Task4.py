f9fbb0ba8feeb35624f60e5eaa21e6ddaf1457b2
joefilvillaruel29 <villarueljopay@gmail.com>
2025-02-26 21:25:59 +0800

    Create Task4.py

---
+++ Task4.py
@@ -0,0 +1,16 @@
+class Book:
+    def __init__(self, title, author, year_published):
+        self.title = title
+        self.author = author
+        self.year_published = year_published
+
+    def describe(self):
+        print(f"'{self.title}' by {self.author}, published in {self.year_published}.")
+
+book1 = Book("To Kill a Mockingbird", "Harper Lee", 1960)
+book2 = Book("1984", "George Orwell", 1949)
+book3 = Book("The Great Gatsby", "F. Scott Fitzgerald", 1925)
+
+book1.describe()
+book2.describe()
+book3.describe()
